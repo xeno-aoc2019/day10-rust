874d898eee235794f7233d6aee61091150badc0d
xeno <xeno.freenode@gmail.com>
2020-01-04 16:21:34 +0100

    split into sectors per pass

---
+++ src/grouping.rs
@@ -0,0 +1,72 @@
+extern crate num;
+use num::integer;
+
+pub fn group_surrounding_stars(station: (i32, i32), coords: Vec<(i32, i32)>) -> Vec<(i32, (i32, i32), (i32, i32), i32)> {
+    let mut grouped = vec!();
+    for star in coords {
+        let dist = distance(station, star);
+        if !(dist == (0, 0)) {
+            let norm_dist = normalized_dist(station, star);
+            let id = star.0 * 100 + star.1;
+            // println!("normalized: ({},{})", norm_dist.0, norm_dist.1);
+            grouped.push((sector(norm_dist), norm_dist, dist, id));
+        }
+    }
+    grouped.sort();
+    grouped
+}
+
+fn sector(star: (i32, i32)) -> i32 {
+    if star.0 == 0 && star.1 == 0 {
+        panic!("trying to find the sector of origo");
+    }
+
+    let x = star.0;
+    let y = star.1;
+    if x == 0 && y > 0 {
+        return 1; // straight up - first "sector"
+    }
+    if y == 0 && x > 0 {
+        return 4; // straight east - fourth "sector"
+    }
+    if x == 0 && y < 0 {
+        return 7; // straight south - seventh "sector"
+    }
+    if y == 0 && x < 0 {
+        return 10; // straight west - 10th "sector"
+    }
+
+    let ys = y / y.abs();
+    let xs = x / x.abs();
+    let xgty = (x.abs() - y.abs()) > 0;
+    let sector = match (ys, xs, xgty) {
+        (1, 1, false) => 2, // nne (northnortheast)
+        (1, 1, true) => 3, // nee
+        (-1, 1, true) => 5, // see
+        (-1, 1, false) => 6, // sse
+        (-1, -1, false) => 8, // ssw
+        (-1, -1, true) => 9, // sww
+        (1, -1, true) => 11,  // nww
+        (1, -1, false) => 12, // nnw
+        zot => {
+            println!("({},{},{})", zot.0, zot.1, zot.2);
+            panic!("Should not happen");
+            5000
+        }
+    };
+    return sector;
+}
+
+fn distance(station: (i32, i32), star: (i32, i32)) -> (i32, i32) {
+    (star.0 - station.0, star.1 - station.1)
+}
+
+fn normalized_dist(station: (i32, i32), star: (i32, i32)) -> (i32, i32) {
+    let dist = distance(station, star);
+    let gcd = integer::gcd(dist.0, dist.1);
+    if gcd == 0 {
+        (0, 0)
+    } else {
+        (dist.0 / gcd, dist.1 / gcd)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,59 @@
 extern crate num;
 
-use num::integer;
+// use num::integer;
 
 mod input;
 mod station;
+mod grouping;
+
+type Coordinate = (i32,i32);
+type Id = i32;
 
 fn main() {
     let coords = input::read_points("input.txt");
     let (station, stars_in_sight) = station::find_station(&coords);
     println!("Station is at ({},{}) with line of sight to {} other stars", station.0, station.1, stars_in_sight);
-    group(station, coords);
-}
-
-fn group(station: (i32, i32), coords: Vec<(i32, i32)>) -> () {
-    let mut grouped = vec!();
-    for star in coords {
-        let dist = distance(station, star);
-        let norm_dist = normalized_dist(station, star);
-        let id = star.0 * 100 + star.1;
-        // println!("normalized: ({},{})", norm_dist.0, norm_dist.1);
-        grouped.push((norm_dist, dist, id));
+    let grouped = grouping::group_surrounding_stars(station, coords);
+    for g in grouped.clone() {
+        // println!("normalized: [{}] ({},{}) [({},{}], {}", g.0, (g.1).0, (g.1).1, (g.2).0, (g.2).1, g.3);
     }
-    grouped.sort();
-    for g in grouped {
-        println!("normalized: ({},{}) [({},{}], {}", (g.0).0, (g.0).1, (g.1).0, (g.1).1, g.2);
+    let sectors = next_rotation_sectors(&grouped);
+    println!("sector_passes = {}", sectors.len());
+    for sect in sectors {
+        println!("*** NEXT PASS: ");
+        for g in sect {
+            println!("sect member: [{}] ({},{}) [({},{}], {}", g.0, (g.1).0, (g.1).1, (g.2).0, (g.2).1, g.3);
+        }
     }
 }
 
-fn distance(station: (i32, i32), star: (i32, i32)) -> (i32,i32) {
-    (star.0 - station.0, star.1 - station.1)
-}
-
-fn normalized_dist(station: (i32, i32), star: (i32, i32)) -> (i32, i32) {
-    let dist = distance(station, star);
-    let gcd = integer::gcd(dist.0, dist.1);
-    if gcd == 0 {
-        (0,0)
-    } else {
-        (dist.0 / gcd, dist.1 / gcd)
+fn next_rotation_sectors(sorted_star_info: &Vec<(Id,Coordinate,Coordinate,Id)>) -> Vec<Vec<(Id,Coordinate,Coordinate,Id)>> {
+    let mut next_rotations = vec!();
+    let mut sectors = vec!();
+    let mut last_norm_vect = (0, 0);
+    let mut current_sector = 1;
+    let mut sector_stars = vec!();
+    for star_info in sorted_star_info {
+        if star_info.1 == last_norm_vect {
+            // println!("    pushing to next rotation: [{}] ({},{})", star_info.0, (star_info.1).0, (star_info.1).1);
+            next_rotations.push(star_info.clone());
+        } else {
+            if current_sector != star_info.0 {
+               // println!("  new sector for current sector: [{}] ({},{})", star_info.0, (star_info.1).0, (star_info.1).1);
+                sectors.push(sector_stars);
+                sector_stars=vec!();
+                current_sector = star_info.0;
+            }
+            // println!("adding to current sector: [{}] ({},{})", star_info.0, (star_info.1).0, (star_info.1).1);
+            sector_stars.push(star_info.clone());
+            last_norm_vect = star_info.1
+        }
     }
+    sectors.push(sector_stars);
+    if next_rotations.len() > 0 {
+        for sector in next_rotation_sectors(&next_rotations) {
+            sectors.push(sector);
+        }
+    }
+    sectors
 }
