18f988a23f84836203f30bd42a124238a7f42ef4
xeno <xeno.freenode@gmail.com>
2020-01-04 08:42:03 +0100

    solved part 1

---
+++ src/input.rs
@@ -0,0 +1,42 @@
+use std::path::Path;
+use std::fs::File;
+use std::io::{Lines, Result, BufRead, BufReader};
+
+
+fn to_coords(lines: Vec<String>) -> Vec<(i32, i32)> {
+    let mut v = vec!();
+    for (y, line) in lines.iter().enumerate() {
+        for (x, c) in line.chars().enumerate() {
+            if c == '#' {
+                v.push((x as i32, y as i32));
+                println!("({},{})", x, y);
+            }
+        }
+    }
+    v
+}
+
+pub fn read_points<P>(file_name: P) -> Vec<(i32, i32)> where P: AsRef<Path> {
+    let lines = read_lines(file_name);
+    to_coords(lines)
+}
+
+fn read_lines<P>(file_name: P) -> Vec<String> where P: AsRef<Path>, {
+    let mut line_vector = vec!();
+    let linesResult = lines(file_name);
+    if let Ok(lines) = linesResult {
+        for lineResult in lines {
+            if let Ok(line) = lineResult {
+                println!("{}", &line);
+                line_vector.push(line);
+            }
+        }
+    }
+    line_vector
+}
+
+fn lines<P>(file_name: P) -> Result<Lines<BufReader<File>>>
+    where P: AsRef<Path>, {
+    let file = File::open(file_name)?;
+    Ok(BufReader::new(file).lines())
+}
